74fe9d2e7a7a4b99f0a8c34b57c4947d595a704c
KeaMeng <2023484ing@aupp.edu.kh>
2025-04-06 14:10:27 +0700

    test production

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(cors()); // Enable CORS
 app.use(express.json({ limit: "10mb" })); // Adjust the limit as needed
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 
